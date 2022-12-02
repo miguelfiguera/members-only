9ed96929983159c1654b955bc5f2ef923cf1a66c
Miguel Figuera <miguelqui725@gmail.com>
2022-12-02 00:33:24 -0400

    log07, inserting authors names on a partial to render on posts#index

--- db/migrate/20221202035507_adding_name.rb
+++ db/migrate/20221202035507_adding_name.rb
@@ -1,5 +1,5 @@
 class AddingName < ActiveRecord::Migration[7.0]
   def change
-    add_column :user, :username, :string
+    add_column :users, :username, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_032414) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_035507) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_032414) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
