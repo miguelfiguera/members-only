1ef2d2a50fda2e4322e90718f8c759075e964bfc
Miguel Figuera <miguelqui725@gmail.com>
2022-12-01 22:28:57 -0400

    log04, creating the user log in, and read only

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,33 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post=Post.new
     end
 
     def create
+        @post=Post.new(post_params)
+        if @post.save
+            redirect_to index_post_path
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def edit
+        @post=Post.find(params[:id])
     end
 
     def update
+        @post=Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render :edit
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title,:body,:user_id)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
     has_many :comments
     belongs_to :User
+
+    validates :body, presence: :true
+    validates :title, presence: :true
+    validates :user_id, presence: :true
 end
